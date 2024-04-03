1909313927c986edad0d9ff66af911b3088f28e2
dmitrii_koriakov_mac <dm.korykov@gmail.com>
2024-04-03 16:04:54 +0200

    added object_track_id

--- object-tracking/process.py
+++ object-tracking/process.py
@@ -8,7 +8,7 @@ def hook(frame_data, context):
     tracker = context['tracker']
     frame = frame_data['modified']
     bboxes, scores, labels = frame_data['user_data'].values()
-    norfair_detections = yolo_to_norfair(bboxes, scores)
+    norfair_detections = yolo_to_norfair(bboxes, scores, labels)
     tracked_objects = tracker.update(detections=norfair_detections)
     draw_points(frame, drawables=tracked_objects)
     frame_data['modified'] = frame
@@ -32,12 +32,12 @@ def get_user_data_from_tracker(tracked_objects):
     return bboxes, scores, labels, obj_track_ids
 
 
-def yolo_to_norfair(bboxes, scores):
+def yolo_to_norfair(bboxes, scores, labels):
     norfair_detections = []
     for i, bbox in enumerate(bboxes):
         box_corners = [[bbox[0], bbox[1]], [bbox[2], bbox[3]]]
         box_corners = np.array(box_corners)
         corners_scores = np.array([scores[i], scores[i]])
-        norfair_detections.append(Detection(points=box_corners, scores=corners_scores))
+        norfair_detections.append(Detection(points=box_corners, scores=corners_scores, label=labels[i]))
 
     return norfair_detections
