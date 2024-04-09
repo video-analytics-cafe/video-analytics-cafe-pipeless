8a2884384ecbb35ab68ecf65ec58ebae6f3f85dd
dmitrii_koriakov_mac <dm.korykov@gmail.com>
2024-04-09 18:30:14 +0200

    debug

--- start-stream/onnx_yolo_object_tracker_kafka.sh
+++ start-stream/onnx_yolo_object_tracker_kafka.sh
@@ -1,3 +1,3 @@
-curl -X POST http://127.0.0.1:3033/streams \
+curl -X POST http://127.0.0.1:3030/streams \
      -H "Content-Type: application/json" \
-     -d '{"input_uri": "file:///input/demo-video-cafe.mp4", "output_uri": "file:///output/demo-video-cafe-output.mp4", "frame_path": ["onnx-yolo", "object-tracking", "kafka-produc"], "restart_policy": "never"}'
+     -d '{"input_uri": "file:///input/demo-video-cafe.mp4", "output_uri": "stream", "frame_path": ["onnx-yolo", "object-tracking", "kafka-produc"], "restart_policy": "never"}'
