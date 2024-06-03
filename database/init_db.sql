6f47e878ffcbee941a81a04605d385cb6941a426
Dmitrii Koriakov <dmitrii.koriakov@uni.lu>
2024-06-03 19:13:01 +0200

    minor env fix

--- database/init_db.sql
+++ database/init_db.sql
@@ -17,8 +17,7 @@ CREATE TABLE IF NOT EXISTS logs (
     down_coords INTEGER,
     created_at TIMESTAMP WITH TIME ZONE,
     PRIMARY KEY (ids, msg_datetime)
-)
-;
+);
 
 -- INSERT INTO logs (ids, msg_datetime, obj_track_id, labels, scores, left_coords, upper_coords, right_coords, down_coords) VALUES
 -- ('98639d3c-debe-4f66-996b-cab2e595188c', '2024-05-24T00:51:34.444709', 8, 'person', 0.478506, 746, 404, 951, 715),
