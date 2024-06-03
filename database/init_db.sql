16b9170997910b5ad36d29914d63b64c2e205e53
Dmitrii Koriakov <dmitrii.koriakov@uni.lu>
2024-06-03 19:07:13 +0200

    minor env fix

--- database/init_db.sql
+++ database/init_db.sql
@@ -1,5 +1,5 @@
-SELECT 'CREATE DATABASE mydb'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'mydb')\gexec
+SELECT 'CREATE DATABASE postgres'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'postgres')\gexec
 \c mydb
 -- CREATE DATABASE IF not EXISTS EXISTS;
 -- USE logs_db;
@@ -20,8 +20,8 @@ CREATE TABLE IF NOT EXISTS logs (
 )
 ;
 
-INSERT INTO logs (ids, msg_datetime, obj_track_id, labels, scores, left_coords, upper_coords, right_coords, down_coords) VALUES
-('98639d3c-debe-4f66-996b-cab2e595188c', '2024-05-24T00:51:34.444709', 8, 'person', 0.478506, 746, 404, 951, 715),
-('98639d3c-debe-4f66-996b-cab2e526373d', '2024-05-24T00:51:34.444710', 8, 'person', 0.745102, 746, 405, 952, 714),
-('98639d3c-dlhe-4f66-736b-cab2e595188c', '2024-05-24T00:51:34.444711', 8, 'person', 0.958475, 746, 406, 953, 713)
-;
+-- INSERT INTO logs (ids, msg_datetime, obj_track_id, labels, scores, left_coords, upper_coords, right_coords, down_coords) VALUES
+-- ('98639d3c-debe-4f66-996b-cab2e595188c', '2024-05-24T00:51:34.444709', 8, 'person', 0.478506, 746, 404, 951, 715),
+-- ('98639d3c-debe-4f66-996b-cab2e526373d', '2024-05-24T00:51:34.444710', 8, 'person', 0.745102, 746, 405, 952, 714),
+-- ('98639d3c-dlhe-4f66-736b-cab2e595188c', '2024-05-24T00:51:34.444711', 8, 'person', 0.958475, 746, 406, 953, 713)
+-- ;
